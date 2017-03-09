bfa8f26248e8f88826d1b8ad2389e81887af2bfb
Quan Le Anh <quanleanhqla@org.com>
2017-03-09 21:25:49 +0700

    add delete and put

--- resources/task_resource.py
+++ resources/task_resource.py
@@ -12,12 +12,14 @@ class TaskListRes(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument(name="name", type=str, location="json")
         parser.add_argument(name="local_id", type=str, location="json")
+        parser.add_argument(name="done", type=str, location="json")
         body = parser.parse_args()
 
         name = body["name"]
         local_id = body["local_id"]
+        done = body["done"]
 
-        task = Task(name=name, local_id=local_id, done=False)
+        task = Task(name=name, local_id=local_id, done=done)
 
         task.save()
 
@@ -28,3 +30,31 @@ class TaskRes(Resource):
     def get(self, task_id):
         task = Task.objects().with_id(task_id)
         return mlab.item2json(task)
+
+class TaskDelRes(Resource):
+    def delete(self, task_id):
+        task = Task.objects().with_id(task_id)
+        if task == None:
+            return {"message": "Task not found"}
+        else:
+            task.delete()
+            return {"message": "Deleted successfully"}
+
+class TaskUpdateRes(Resource):
+    def put(self, task_id):
+        task = Task.objects().with_id(task_id)
+
+        parser = reqparse.RequestParser()
+        parser.add_argument(name="name", type=str, location="json")
+        parser.add_argument(name="local_id", type=str, location="json")
+        parser.add_argument(name="done", type=str, location="json")
+        body = parser.parse_args()
+
+        name = body["name"]
+        local_id = body["local_id"]
+        done = body["done"]
+
+        task.update(name=name, local_id=local_id, done=done)
+
+        updated_task = Task.objects().with_id(task.id)
+        return mlab.item2json(updated_task)
